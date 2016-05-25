a9b53122d98fe2fcfc2016035a74cbb12bf95e1d
richard <lichen.peng@gmail.com>
2016-05-25 14:03:20 +0800

    changed to pamela

--- http-server.js
+++ http-server.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('humphrey: ' + new Date() + '\n');
+  res.end('pamela: ' + new Date() + '\n');
 }).listen(port);
 
 console.log('Server running on port %s', port);
