b3eb2d8891d1f051b238f77b61faebead099e364
richard <lichen.peng@gmail.com>
2016-05-25 11:17:03 +0800

    prefix changed

--- http-server.js
+++ http-server.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('richard: ' + new Date() + '\n');
+  res.end('daphne: ' + new Date() + '\n');
 }).listen(port);
 
 console.log('Server running on port %s', port);
