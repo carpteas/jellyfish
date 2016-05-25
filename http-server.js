84f673e0df35259c33a3fc513637cff3f22ee779
richard <lichen.peng@gmail.com>
2016-05-25 10:28:59 +0800

    node.js demo

---
+++ http-server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(new Date() + '\n');
+}).listen(port);
+
+console.log('Server running on port %s', port);
