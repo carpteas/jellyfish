1fd8848072d3244b237a0ae38569cc817138a069
richard <lichen.peng@gmail.com>
2016-06-01 10:34:19 +0800

    cleanup

--- http-server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var port = process.env.PORT || 1337;
-
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('pamela: ' + new Date() + '\n');
-}).listen(port);
-
-console.log('Server running on port %s', port);
