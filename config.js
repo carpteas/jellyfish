348060fd699be350e8a40ebcec398ef40de7fda8
richard <lichen.peng@gmail.com>
2016-06-05 15:58:21 +0800

    socket.io for postback

--- config.js
+++ config.js
@@ -7,14 +7,17 @@ module.exports = {
   'awsAccess': 'AKIAJQ75TXVUWG3LPMVQ',
   'awsRegion': 'ap-northeast-1',
   'awsSecret': '',
+  'backwss': '',
   'database': 'mongodb://localhost:27017/test',
   'expiresIn': '30m',
+  'host': 'http://jellyfish.carpteas.com',
   'hmac': 'sha256',
   'logError': './jellyfish-error.log',
   'logLevel': 'error',
   'readRegex': /^\/\basset\b\/([\w\.~-]+)\/([\w\.~-]+)\/(.*)/,
   'writeRegex': /^\/\bapi\b\/([\w\.~-]+)\/([\w\.~-]+)\/(.*)/,
   'port': 8888,
+  'redis': 'localhost:6379',
   'secret': uuid.v4(),
   's3Vanish': 30
 };
