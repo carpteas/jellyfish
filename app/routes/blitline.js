98a84f07a41edad9df3a1f3440313a150d808563
richard <lichen.peng@gmail.com>
2016-06-06 17:08:36 +0800

    debugging simple_blitline_node ...

--- app/routes/blitline.js
+++ app/routes/blitline.js
@@ -5,24 +5,18 @@ var restify         = require('restify');
 var util            = require('util.js');
 
 module.exports = function(req, res, next) {
-var json = JSON.parse(req.params['results']);
-util.emitter.emit('audit', req.params['results']);
-util.emitter.emit('audit', json.job_id);
-return next(res.send({ success: true }));
-/*
   try {
-    var result = JSON.parse(req.body).results;
-    if (!Boolean(result) || !Boolean(result.job_id)) throw new Error('syntax bad');
+    var json = JSON.parse(req.params['results']);
+    if (!Boolean(json.job_id)) throw new Error('id missing');
 
-    if (!Boolean(result.errors)) {
-      util.emitter.emit('success', result.job_id);
+    if (!Boolean(json.errors)) {
+      util.emitter.emit('success', json.job_id);
     } else {
-      util.emitter.emit('failure', { job: result.job_id, reason: result.errors[0] });
+      util.emitter.emit('failure', { job: json.job_id, reason: json.errors[0] });
     }
 
     return next(res.send({ success: true }));
   } catch (err) {
     next.ifError(new restify.BadRequestError('request body invalid'));
   }
-*/
 };
